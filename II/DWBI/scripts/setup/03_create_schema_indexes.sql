2d870e64ebcb7d70fa36a024594faf6e9f7e492b
Andrei Murariu <andrei.smurariu@proton.me>
2026-02-11 21:06:18 +0200

    DWBI: Project - Cleanup indexes

--- II/DWBI/scripts/setup/03_create_schema_indexes.sql
+++ II/DWBI/scripts/setup/03_create_schema_indexes.sql
@@ -1,15 +1,2 @@
 ALTER SESSION SET CONTAINER = orclpdb1;
 WHENEVER SQLERROR EXIT FAILURE;
-
-CREATE INDEX idx_ticket_client ON TickLy.ticket(client_id);
-CREATE INDEX idx_ticket_status ON TickLy.ticket(status_id);
-CREATE INDEX idx_ticket_departament ON TickLy.ticket(departament_id);
-CREATE INDEX idx_ticket_categorie ON TickLy.ticket(categorie_id);
-CREATE INDEX idx_ticket_ultima_actualizare ON TickLy.ticket(data_ultima_actualizare);
-CREATE INDEX idx_comment_client_ticket ON TickLy.comment_client(ticket_id);
-CREATE INDEX idx_comment_client_client ON TickLy.comment_client(client_id);
-CREATE INDEX idx_comment_agent_ticket ON TickLy.comment_agent(ticket_id);
-CREATE INDEX idx_comment_agent_agent ON TickLy.comment_agent(agent_id);
-CREATE INDEX idx_ticket_history_ticket ON TickLy.ticket_history(ticket_id);
-CREATE INDEX idx_atasament_ticket ON TickLy.atasament(ticket_id);
-CREATE INDEX idx_adresa_client ON TickLy.adresa(client_id);
